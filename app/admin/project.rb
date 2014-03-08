cbb3e9552b6314390485b87855e9b0a3b989d662
Cesar Navarro Araya <cnaraya01@gmail.com>
2014-03-08 12:02:42 -0600

    List participants for each project

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,6 +1,33 @@
 ActiveAdmin.register Project do
   menu priority: 3
 
-  permit_params :code, :name, :stand, :phase, :motivation, :description, :advantage, :category_id, :judge_id
+  permit_params :code, :name, :stand, :phase, :motivation, :description, :advantage, :category_id, :judge_id, participant_ids: []
+
+  index do
+    column :category
+    column :code
+    column :name
+    column :stand
+    column :phase
+    column "Participantes" do |project|
+      project.participants.map { |p| p.carnet }.join('</br>').html_safe
+    end
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :category_id, as: :select, collection: Category.all.map {|c| [c.name, c.id] }
+      f.input :code
+      f.input :name
+      f.input :motivation
+      f.input :description
+      f.input :advantage
+      f.input :stand
+      f.input :phase
+      f.input :participants, as: :check_boxes, collection: Participant.all, member_label: Proc.new { |p| " #{p.carnet} - #{p.name}" }
+    end
+    f.actions
+  end
 end
 
