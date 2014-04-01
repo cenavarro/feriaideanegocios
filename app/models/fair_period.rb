0f838b9ead8cd5424baa51d9ec77fab0a074e9d7
Cesar Navarro Araya <cnaraya01@gmail.com>
2014-04-01 09:02:04 -0600

    Fixed problem with date on fair period

--- app/models/fair_period.rb
+++ app/models/fair_period.rb
@@ -2,6 +2,6 @@ class FairPeriod < ActiveRecord::Base
 
   def self.can_add_project?
     period, today  = first, Date.today
-    (period.started_at < today && today < first.ended_at)
+    (period.started_at <= today && today <= first.ended_at)
   end
 end
