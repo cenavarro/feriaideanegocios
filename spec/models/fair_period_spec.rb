30c9e9c893d96f8802a4e47db86bc9116fe0f481
Cesar Navarro Araya <cnaraya01@gmail.com>
2014-04-23 20:12:07 -0600

    Fixed test

--- spec/models/fair_period_spec.rb
+++ spec/models/fair_period_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FairPeriod, ".can_add_project" do
-  let!(:fair_period) { FactoryGirl.create(:fair_period, started_at: 1.month.ago, ended_at: 1.day.ago) }
+  let!(:fair_period) { FactoryGirl.create(:fair_period, started_at: 1.month.ago, ended_at: 3.day.ago) }
 
   context "when today is in the available period" do
     before { allow(Date).to receive(:today) { 5.days.ago } }
